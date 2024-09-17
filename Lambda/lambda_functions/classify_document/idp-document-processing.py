1956d7d27ce34f15198273c3e1606aede6f89798
Hugo <hmancini@amazon.com.ar>
2024-09-17 18:23:42 -0300

    added batch processing to comprehend job creation and develop idp-postclass-notification

--- Lambda/lambda_functions/classify_document/idp-document-processing.py
+++ Lambda/lambda_functions/classify_document/idp-document-processing.py
@@ -2,7 +2,7 @@ import json
 import boto3
 import uuid
 import os
-import base64
+from collections import deque
 
 # Initialize AWS clients
 sqs_client = boto3.client('sqs')
@@ -15,80 +15,77 @@ QUEUE_URL = os.environ['QUEUE_URL']
 CLASSIFIER_ARN = os.environ['CLASSIFIER_ARN']
 SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
 DATA_ACCESS_ROLE_ARN = os.environ['DATA_ACCESS_ROLE_ARN']
-
-def get_document_text(bucket, key):
-    response = s3_client.get_object(Bucket=bucket, Key=key)
-    document_content = response['Body'].read()
-    return base64.b64encode(document_content).decode('utf-8')
-
-def format_sns_message(classification_result, bucket, key):
-    return {
-        'bucket': bucket,
-        'input_key': key,
-        'job_name': classification_result['JobName'],
-        'score': classification_result['Labels'][0]['Score']
-    }
+OUTPUT_BUCKET = os.environ['OUTPUT_BUCKET']
 
 def lambda_handler(event, context):
-    # Get messages from SQS queue
-    messages = sqs_client.receive_message(
-        QueueUrl=QUEUE_URL,
-        MaxNumberOfMessages=10  # Adjust this value as per your requirements
-    )
+    document_paths = deque()
 
-    if 'Messages' in messages:
-        for message in messages['Messages']:
-            message_body = json.loads(message['Body'])
+    # Receive messages in batches of 10
+    while True:
+        messages = sqs_client.receive_message(
+            QueueUrl=QUEUE_URL,
+            MaxNumberOfMessages=10
+        )
 
-            # Get information from the message
-            bucket = message_body['bucket']
-            key = message_body['key']
+        if 'Messages' in messages:
+            for message in messages['Messages']:
+                message_body = json.loads(message['Body'])
 
-            try:
-                # Start Comprehend Classification job
-                job_name = f'doc-classification-job-{uuid.uuid4()}'
-                print(f'Starting Comprehend Classification job {job_name} with model {CLASSIFIER_ARN}')
-
-                response = comprehend.start_document_classification_job(
-                    JobName=job_name,
-                    DocumentClassifierArn=CLASSIFIER_ARN,
-                    InputDataConfig={
-                        'S3Uri': f's3://{bucket}/{key}',
-                        'InputFormat': 'ONE_DOC_PER_FILE',
-                        'DocumentReaderConfig': {
-                            'DocumentReadAction': 'TEXTRACT_DETECT_DOCUMENT_TEXT',
-                            'DocumentReadMode': 'FORCE_DOCUMENT_READ_ACTION'
-                        }
-                    },
-                    OutputDataConfig={
-                        'S3Uri': f's3://{bucket}/comprehend/doc-class-output/'
-                    },
-                    DataAccessRoleArn=DATA_ACCESS_ROLE_ARN
-                )
-
-                # Format and publish SNS message
-                sns_message = format_sns_message(response, bucket, key)
-                sns_client.publish(
-                    TopicArn=SNS_TOPIC_ARN,
-                    Message=json.dumps(sns_message)
-                )
+                # Get information from the message
+                bucket = message_body['bucket']
+                key = message_body['key']
+                document_paths.append(f's3://{bucket}/{key}')
 
                 # Delete message from the original queue
                 sqs_client.delete_message(
                     QueueUrl=QUEUE_URL,
                     ReceiptHandle=message['ReceiptHandle']
                 )
+        else:
+            break
 
-            except Exception as e:
-                print(f"Error: {e}")
+    try:
+        # Start Comprehend Classification job
+        job_name = f'doc-classification-job-{uuid.uuid4()}'
+        print(f'Starting Comprehend Classification job {job_name} with model {CLASSIFIER_ARN}')
+
+        comprehend.start_document_classification_job(
+            JobName=job_name,
+            DocumentClassifierArn=CLASSIFIER_ARN,
+            InputDataConfig={
+                'S3Uri': ','.join(document_paths),
+                'InputFormat': 'ONE_DOC_PER_FILE',
+                'DocumentReaderConfig': {
+                    'DocumentReadAction': 'TEXTRACT_DETECT_DOCUMENT_TEXT',
+                    'DocumentReadMode': 'FORCE_DOCUMENT_READ_ACTION'
+                }
+            },
+            OutputDataConfig={
+                'S3Uri': f's3://{OUTPUT_BUCKET}/comprehend/doc-class-output/'
+            },
+            DataAccessRoleArn=DATA_ACCESS_ROLE_ARN
+        )
 
         return {
             'statusCode': 200,
             'body': 'Documents processed successfully'
         }
 
-    else:
-        return {
-            'statusCode': 200,
-            'body': 'No messages in the queue'
-        }
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Job creation failed. Messages written to SNS.")
+        # Format and publish SNS messages
+        for document_path in document_paths:
+            sns_message = {
+                'bucket': document_path.split('/')[2],
+                'key': '/'.join(document_path.split('/')[3:])
+            }
+            sns_client.publish(
+                TopicArn=SNS_TOPIC_ARN,
+                Message=json.dumps(sns_message)
+            )
+
+    return {
+        'statusCode': 200,
+        'body': 'No messages in the queue'
+    }
