7fd949bcd8aa47a17c7eb928f63419b689aa8615
Hugo <hmancini@amazon.com.ar>
2024-09-18 21:09:35 -0300

    automatic classification by put event working 18/09/2024

--- Lambda/lambda_functions/classify_document/idp-document-processing.py
+++ Lambda/lambda_functions/classify_document/idp-document-processing.py
@@ -1,91 +1,126 @@
 import json
 import boto3
-import uuid
 import os
-from collections import deque
+from botocore.exceptions import ClientError
+from datetime import datetime
 
 # Initialize AWS clients
 sqs_client = boto3.client('sqs')
 s3_client = boto3.client('s3')
 comprehend = boto3.client('comprehend')
-sns_client = boto3.client('sns')
+dynamodb = boto3.resource('dynamodb')
 
 # Get environment variables
 QUEUE_URL = os.environ['QUEUE_URL']
 CLASSIFIER_ARN = os.environ['CLASSIFIER_ARN']
-SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
 DATA_ACCESS_ROLE_ARN = os.environ['DATA_ACCESS_ROLE_ARN']
 OUTPUT_BUCKET = os.environ['OUTPUT_BUCKET']
+DYNAMODB_TABLE_NAME = os.environ['DYNAMODB_TABLE_NAME']
+
+def process_message(message_body, table):
+    # Get information from the message
+    bucket = message_body['bucket']
+    key = message_body['key']
+
+    # Extract filename and prefix from the key
+    filename = os.path.basename(key)
+    prefix = os.path.dirname(key)
+
+    # Start Comprehend Classification job
+    job_name = f'document-classification-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+    print(f'Starting Comprehend Classification job {job_name} with model {CLASSIFIER_ARN}')
+
+    # Build an S3 path for the file to be processed
+    s3_uri = os.path.join('s3://', bucket, key)
+
+    comprehend.start_document_classification_job(
+        JobName=job_name,
+        DocumentClassifierArn=CLASSIFIER_ARN,
+        InputDataConfig={
+            'S3Uri': s3_uri,
+            'InputFormat': 'ONE_DOC_PER_FILE',
+            'DocumentReaderConfig': {
+                'DocumentReadAction': 'TEXTRACT_DETECT_DOCUMENT_TEXT',
+                'DocumentReadMode': 'FORCE_DOCUMENT_READ_ACTION'
+            }
+        },
+        OutputDataConfig={
+            'S3Uri': f's3://{OUTPUT_BUCKET}/comprehend/doc-class-output/'
+        },
+        DataAccessRoleArn=DATA_ACCESS_ROLE_ARN
+    )
+
+    # Write to DynamoDB table with put_item
+    table.put_item(
+        Item={
+            'bucket': bucket,
+            'filename': filename,
+            'prefix': prefix,
+            'key': key
+        }
+    )
+
+    print(f"Processed message: {message_body}")
 
 def lambda_handler(event, context):
-    document_paths = deque()
-
-    # Receive messages in batches of 10
-    while True:
-        messages = sqs_client.receive_message(
-            QueueUrl=QUEUE_URL,
-            MaxNumberOfMessages=10
-        )
-
-        if 'Messages' in messages:
-            for message in messages['Messages']:
-                message_body = json.loads(message['Body'])
-
-                # Get information from the message
-                bucket = message_body['bucket']
-                key = message_body['key']
-                document_paths.append(f's3://{bucket}/{key}')
-
-                # Delete message from the original queue
-                sqs_client.delete_message(
-                    QueueUrl=QUEUE_URL,
-                    ReceiptHandle=message['ReceiptHandle']
-                )
-        else:
-            break
+    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
     try:
-        # Start Comprehend Classification job
-        job_name = f'doc-classification-job-{uuid.uuid4()}'
-        print(f'Starting Comprehend Classification job {job_name} with model {CLASSIFIER_ARN}')
-
-        comprehend.start_document_classification_job(
-            JobName=job_name,
-            DocumentClassifierArn=CLASSIFIER_ARN,
-            InputDataConfig={
-                'S3Uri': ','.join(document_paths),
-                'InputFormat': 'ONE_DOC_PER_FILE',
-                'DocumentReaderConfig': {
-                    'DocumentReadAction': 'TEXTRACT_DETECT_DOCUMENT_TEXT',
-                    'DocumentReadMode': 'FORCE_DOCUMENT_READ_ACTION'
-                }
-            },
-            OutputDataConfig={
-                'S3Uri': f's3://{OUTPUT_BUCKET}/comprehend/doc-class-output/'
-            },
-            DataAccessRoleArn=DATA_ACCESS_ROLE_ARN
-        )
-
-        return {
-            'statusCode': 200,
-            'body': 'Documents processed successfully'
-        }
-
-    except Exception as e:
-        print(f"Error: {e}")
-        print("Job creation failed. Messages written to SNS.")
-        # Format and publish SNS messages
-        for document_path in document_paths:
-            sns_message = {
-                'bucket': document_path.split('/')[2],
-                'key': '/'.join(document_path.split('/')[3:])
+        if 'Records' in event:  # This is an SQS trigger event
+            for record in event['Records']:
+                message_body = json.loads(record['body'])
+                process_message(message_body, table)
+                
+                # Delete the processed message from the queue
+                sqs_client.delete_message(
+                    QueueUrl=QUEUE_URL,
+                    ReceiptHandle=record['receiptHandle']
+                )
+            
+            return {
+                'statusCode': 200,
+                'body': f'Processed {len(event["Records"])} messages successfully.'
             }
-            sns_client.publish(
-                TopicArn=SNS_TOPIC_ARN,
-                Message=json.dumps(sns_message)
+        else:  # This is a manual invocation or test event
+            # Receive a single message from the queue
+            message = sqs_client.receive_message(
+                QueueUrl=QUEUE_URL,
+                MaxNumberOfMessages=1,
+                WaitTimeSeconds=5
             )
 
-    return {
-        'statusCode': 200,
-        'body': 'No messages in the queue'
-    }
+            if 'Messages' in message:
+                message_body = json.loads(message['Messages'][0]['Body'])
+                receipt_handle = message['Messages'][0]['ReceiptHandle']
+
+                process_message(message_body, table)
+
+                # Delete the processed message from the queue
+                sqs_client.delete_message(
+                    QueueUrl=QUEUE_URL,
+                    ReceiptHandle=receipt_handle
+                )
+
+                return {
+                    'statusCode': 200,
+                    'body': f'Document processed successfully. Message: {message_body}'
+                }
+            else:
+                print("No messages in the queue")
+                return {
+                    'statusCode': 200,
+                    'body': 'No messages in the queue.'
+                }
+
+    except ClientError as e:
+        print(f"AWS ClientError: {e}")
+        return {
+            'statusCode': 500,
+            'body': f'Error processing messages: AWS ClientError. {str(e)}'
+        }
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return {
+            'statusCode': 500,
+            'body': f'Error processing messages: Unexpected error. {str(e)}'
+        }
